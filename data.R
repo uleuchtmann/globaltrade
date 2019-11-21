f2e157e94b5d46b6c7ceb90872ced7f1f2459369
Ulrich Leuchtmann <ulrich.leuchtmann@t-online.de>
2019-11-21 12:10:13 +0100

    added analysis of EMU imports from Japan

--- data.R
+++ data.R
@@ -1,6 +1,4 @@
 library(rdbnomics)
-library(xts)
-library(countrycode)
 source("utilities.R")
 countries <- read.csv("countriesDOT.csv", na.strings ="#N/A", stringsAsFactors = FALSE)
 
@@ -12,6 +10,11 @@ jp.exports.emu <- dot2panel("JP", countries[countries$is_country & countries$eur
                             indicator = "exp", freq ="A", 
                             startDate = "2018-01-01", endDate = "2018-01-01")
 
+emu.imports.jp <- dot2panel(countries[countries$is_country & countries$euro_area, "dotcode"], "JP",
+                            indicator = "imp", freq = "A",
+                            startDate = "2018-01-01", endDate = "2018-01-01")
+
+
 emu.gdp <- weo2panel(countries[countries$is_country & countries$euro_area, "weocode"], 
                      "NGDP", startDate = "2018-01-01", endDate = "2018-01-01")
 
@@ -20,6 +23,17 @@ jp.exp <- merge(emu.gdp, merge(jp.exports.emu, countries[,c("dotcode", "weocode"
                 by.x = "country", by.y = "weocode", all = TRUE)[, c("country", "NGDP", "value")]
 names(jp.exp) <- c("country", "GDP", "exports")
 
+
+emu.imp <- merge(emu.gdp, merge(emu.imports.jp, countries[,c("dotcode", "weocode")], 
+                                by.x = "origin", by.y = "dotcode", all.x = TRUE)[,c("weocode", "value")],
+                 by.x = "country", by.y = "weocode", all = TRUE)[,c("country", "NGDP", "value")]
+names(emu.imp) <- c("country", "GDP", "imports")
+
+
 plot(exports ~ GDP, data = jp.exp)          
 with(jp.exp, text(exports ~ GDP, labels = country, pos = 4))         
-          
+
+plot(imports ~ GDP, data = emu.imp)          
+with(emu.imp, text(imports ~ GDP, labels = country, pos = 4))         
+
+
